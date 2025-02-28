8f8fb2f69254000e5ff82fb8fc404a64ffc11b5c
Brent den Boer <brent@wittig.nl>
2025-02-28 10:52:48 +0100

    solved removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = () => {};
+const removeFromArray = (arrayOfNumbers, ...toRemove) => {
+  for (const number of toRemove) {
+    let numberIndex = arrayOfNumbers.indexOf(number);
+
+    while (numberIndex !== -1) {
+      arrayOfNumbers.splice(numberIndex, 1);
+
+      numberIndex = arrayOfNumbers.indexOf(number);
+    }
+  }
+
+  return arrayOfNumbers;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
