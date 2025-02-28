360a2b8c6d1c1c42c79b1b15999c5a29313a77c9
Brent den Boer <brent@wittig.nl>
2025-02-28 10:52:33 +0100

    solved reverseSring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,12 @@
 const reverseString = (string) => {
   const stringLength = string.length;
-
   let newString = "";
-  for (let i = 0; stringLength > i; i++) {
+
+  for (let i = 1; stringLength >= i; i++) {
     newString += string.at(-i);
   }
-  // return string.split("").reverse().join("");
+
+  return newString;
 };
 
 // Do not edit below this line
