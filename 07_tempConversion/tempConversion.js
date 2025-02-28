09e59623f8493ca02a2ac7b81056dbfb1ab6860b
Brent den Boer <brent@wittig.nl>
2025-02-28 14:17:50 +0100

    Solved tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,10 @@
-const convertToCelsius = () => {};
+const convertToCelsius = (fahrenheit) => {
+  return Number.parseFloat(((fahrenheit - 32) / 1.8).toFixed(1));
+};
 
-const convertToFahrenheit = () => {};
+const convertToFahrenheit = (celcius) => {
+  return Number.parseFloat((celcius * 1.8 + 32).toFixed(1));
+};
 
 // Do not edit below this line
 module.exports = {
